527277ce4833d93851a29cfb344b69b4bb48e6cf
ocpu <martin.hovre@opencubes.io>
2018-06-14 12:59:30 +0200

    Changed from datetime to dateTime to match properties

--- components/post.jsx
+++ components/post.jsx
@@ -11,7 +11,7 @@ export default class Post extends React.Component {
       <article>
         <header className={css.header}>
           <Link href={`/posts/${this.props.id}`} as={`/posts/${this.props.title}`}><h2>{this.props.title}</h2></Link>
-          <date datetime={this.props.modified}>{moment(new Date(this.props.modified)).format('YYYY-MM-DD')}</date>
+          <date dateTime={this.props.modified}>{moment(new Date(this.props.modified)).format('YYYY-MM-DD')}</date>
         </header>
         <p>{this.props.content}</p>
       </article>
